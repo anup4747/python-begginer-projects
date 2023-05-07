ad29bd47782aa7ba972ace2a68e44f4508feeede
Anup Tarwade <119606225+anup4747@users.noreply.github.com>
2023-05-07 23:33:06 +0530

    Create password.py
    new project

---
+++ password.py
@@ -0,0 +1,14 @@
+import random
+import string
+
+# Define the length of the password
+password_length = 10
+
+# Define the characters to use in the password
+password_characters = string.ascii_letters + string.digits + string.punctuation
+
+# Generate the password
+password = ''.join(random.choice(password_characters) for i in range(password_length))
+
+# Print the password to the console
+print("Your random password is:", password)
